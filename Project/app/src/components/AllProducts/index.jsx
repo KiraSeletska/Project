5ea077dffdb0fd159ffdef9f7833fb514a2fc019
KiraSeletska <seletskaya.kira@gmail.com>
2023-07-09 20:55:30 +0200

    .

--- Project/app/src/components/AllProducts/index.jsx
+++ Project/app/src/components/AllProducts/index.jsx
@@ -1,14 +1,16 @@
 import { useGetAllPropductsQuery } from "../../redux/apiSlice";
 import { Product } from "../Product";
 import { baseUrl } from "../../redux/apiSlice";
+import styles from './products.module.css'
+
 
 export const AllProducts = () => {
   const { data } = useGetAllPropductsQuery();
  // console.log(data);
 
   return (
-    <div>
-        IM propducts!
+    <div className={styles.productsWrapper}>
+    
       {data &&
         data.map((el) => (
           <Product
