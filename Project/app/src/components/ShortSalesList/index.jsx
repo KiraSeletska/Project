ead8ccfc493a0a505b72a3b9a1cbefc13f8cce85
KiraSeletska <seletskaya.kira@gmail.com>
2023-07-17 16:48:09 +0200

    .

---
+++ Project/app/src/components/ShortSalesList/index.jsx
@@ -0,0 +1,55 @@
+import styles from "./styles.module.css";
+import { useGetAllPropductsQuery } from "../../redux/apiSlice";
+import { baseUrl } from "../../redux/apiSlice";
+import { Product } from "../Product";
+import { useState } from "react";
+
+export const ShortSalesList = () => {
+  const { data } = useGetAllPropductsQuery();
+  const [productsState, setProductsState] = useState(40);
+
+  const oneStep = 600;
+
+  const stepPlus = () => {
+    if (productsState < -2360) return;
+    const step = productsState - oneStep;
+    setProductsState(step);
+    console.log(productsState)
+  };
+  const stepMin = () => {
+    if (productsState >=40) return;
+    const step = productsState + oneStep;
+    setProductsState(step);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <h2>Sale</h2>
+      <div className={styles.productsWrapper}>
+        <button className={styles.btn} onClick={() => stepPlus()}>
+          {">"}
+        </button>
+        <div
+          className={styles.prodacts}
+          style={{ transform: `translateX(${productsState}px)` }}
+        >
+          {data &&
+            data.map((el) =>
+              el.discont_price ? (
+                <Product
+                  key={el.id}
+                  discont_price={el.discont_price}
+                  price={el.price}
+                  title={el.title}
+                  image={baseUrl + el.image}
+                />
+              ) : (
+                ""
+              )
+            )}
+        </div>
+        <button onClick={() => stepMin()}>{"<"}</button>
+      </div>
+    </div>
+  );
+};
--- Project/app/src/components/mainPage/index.jsx
+++ Project/app/src/components/mainPage/index.jsx
@@ -1,12 +1,15 @@
 import { Sales } from "../Sales"
 import { SmallCataloge } from "../SmallCatalog"
 import { CuponForm } from "../CuponForm"
+import { ShortSalesList} from '../ShortSalesList'
+
 export const MainPage = () => {
     return (
 <div>
 <Sales/>
 <SmallCataloge/>
   <CuponForm/>
+  < ShortSalesList/>
 </div>
     )
     }
